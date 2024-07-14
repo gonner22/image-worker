101117317c17572770e6db945dd7d9d87f3e6500
mrschmiklz <33757454+mrschmiklz@users.noreply.github.com>
2024-07-14 15:23:39 -0600

    Update horde-bridge.cmd

--- horde-bridge.cmd
+++ horde-bridge.cmd
@@ -1,9 +1,24 @@
 @echo off
 cd /d %~dp0
 
-: This first call to runtime activates the environment for the rest of the script
+:: Check if AI_HORDE_URL is set and if it is not, set it
+if "%AI_HORDE_URL%"=="" (
+    echo Setting AI_HORDE_URL environment variable
+    set AI_HORDE_URL=https://api.aipowergrid.io/api/
+) else (
+    echo AI_HORDE_URL is already set to %AI_HORDE_URL%
+)
+
+:: Ask for GPU selection
+echo Select which GPU to use by index (e.g., 0, 1, 2...):
+set /p GPU_INDEX="Enter GPU index: "
+set CUDA_VISIBLE_DEVICES=%GPU_INDEX%
+echo Using GPU %CUDA_VISIBLE_DEVICES%
+
+:: This first call to runtime activates the environment for the rest of the script
 call runtime python -s -m pip -V
 
+:: Uninstall hordelib and install updated packages
 call python -s -m pip uninstall hordelib
 call python -s -m pip install horde_sdk~=0.10.0 horde_model_reference~=0.6.3 horde_engine~=2.11.1 horde_safety~=0.2.3 -U
 
@@ -11,6 +26,10 @@ if %ERRORLEVEL% NEQ 0 (
     echo "Please run update-runtime.cmd."
     GOTO END
 )
+
+:: Change constants in path_consts.py
+echo Modifying path_consts.py to update repository details...
+call python update_path_consts.py
 
 call python -s -m pip check
 if %ERRORLEVEL% NEQ 0 (
